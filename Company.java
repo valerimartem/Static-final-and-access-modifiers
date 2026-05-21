c69e90e7cce6d67b41459ab251bf58490d4af101
valerimartem <valerimartem@gmail.com>
2026-05-21 20:02:30 +0300

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,32 @@
+package practice3;
+
+public class Company {
+    //Создайте класс Company с полями:
+    //static String companyName — общее название для всех сотрудников
+    //final int employeeID — уникальный идентификатор (нельзя менять)
+    //String employeeName — имя сотрудника
+    // Реализуйте конструктор, принимающий employeeID и employeeName,
+    // статический метод printCompanyName(), геттеры и сеттеры для employeeName.
+    // В main: создайте несколько сотрудников, измените companyName и проверьте, что она изменилась для всех.
+    // Попробуйте изменить employeeID — должно быть невозможно.
+
+    static String companyName;
+    final int employeeID;
+    String employeeName;
+
+    Company(int employeeID, String employeeName) {
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+    }
+
+    static void printCompanyName() {
+        System.out.println("Имя компании: " + Company.companyName);
+    }
+    String getEmployeeName() {
+        return this.employeeName;
+    }
+    void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+}
---
+++ GameSettings.java
@@ -0,0 +1,30 @@
+package practice3;
+
+public class GameSettings {
+    //Создайте класс GameSettings с полями:
+    //static int maxPlayers — общее ограничение игроков
+    //final String gameName — название (нельзя менять)
+    //int currentPlayers — сколько игроков в игре сейчас
+    // Реализуйте конструктор, статический метод setMaxPlayers(int),
+    // метод addPlayer() — добавляет 1 игрока,
+    // метод printGameStatus() — выводит название, текущее и максимальное количество игроков.
+    // В main: создайте 2 игры, измените maxPlayers, добавьте игроков и выведите статус.
+
+    static int maxPlayers;
+    final String gameName;
+    int currentPlayers;
+
+    GameSettings(String gameName, int currentPlayers) {
+        this.gameName = gameName;
+        this.currentPlayers = currentPlayers;
+    }
+    static void setMaxPlayers(int someMaxPlayers) {
+        maxPlayers = someMaxPlayers;
+    }
+    void addPlayer() {
+        this.currentPlayers++;
+    }
+    void printGameStatus() {
+        System.out.println("Название игры: " + this.gameName + " Текущее количество игроков: " + this.currentPlayers + " Максимум игроков: " + GameSettings.maxPlayers);
+    }
+}
